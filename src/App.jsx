8d87312d2931d4a8fe129282acb3187ecde48955
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-04-03 15:58:52 +0300

    Add Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 // import views
 import Login from './screens/Login';
 
+// import components
+import Header from './components/UI/Header';
+
 // import styles
 import './sass/index.scss';
 
 const App = () => {
-  return <Login />;
+  return (
+    <>
+      <Header />
+      <Login />
+    </>
+  );
 };
 
 export default App;
