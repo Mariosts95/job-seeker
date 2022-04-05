b066ad266bf07aa5fa11eeb3f49b531c6a1eb782
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-04-05 21:52:39 +0300

    Add basic form handling on login form

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,3 +1,5 @@
+import { useRef, useState, useEffect } from 'react';
+
 // import styles
 import './loginForm.scoped.scss';
 
@@ -6,8 +8,45 @@ import Button from '../UI/Button';
 import Input from '../UI/Input';
 
 const LoginForm = ({ onClick }) => {
+  const emailRef = useRef();
+  const passwordRef = useRef();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [emailError, setEmailError] = useState('');
+  const [passwordError, setPasswordError] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  // set focus on email input
+  useEffect(() => {
+    emailRef.current.focus();
+  }, []);
+
+  // clear email error message on input change
+  useEffect(() => {
+    setEmailError('');
+    setErrorMsg('');
+  }, [email]);
+
+  // clear password error message on input change
+  useEffect(() => {
+    setPasswordError('');
+    setErrorMsg('');
+  }, [password]);
+
+  // handle form submit
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    ValidateEmail(email);
+    console.log(ValidateEmail(email));
+    console.log(password);
+    setEmailError('Email Error');
+    setPasswordError('Password Error');
+    setErrorMsg('Error Message');
+  };
+
   return (
-    <form className='login-form'>
+    <form className='login-form' onSubmit={handleSubmit}>
       <div className='inputs-container'>
         <Input
           type='email'
@@ -15,27 +54,30 @@ const LoginForm = ({ onClick }) => {
           placeholder='Email'
           labelText='Enter your email:'
           autoComplete='off'
+          required
+          hasError={emailError}
+          refValue={emailRef}
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
         />
+        {emailError && <div className='error-msg'>{emailError}</div>}
         <Input
           type='password'
           inputId='password'
           placeholder='Password'
           labelText='Enter your password:'
+          required
+          hasError={passwordError}
+          refValue={passwordRef}
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
         />
+        {passwordError && <div className='error-msg'>{passwordError}</div>}
       </div>
       <div className='submit-container'>
-        <Button
-          type='submit'
-          onClick={(e) => {
-            e.preventDefault();
-            console.log(document.getElementById('email').value);
-            console.log(document.getElementById('password').value);
-            onClick();
-          }}
-        >
-          Login
-        </Button>
+        <Button type='submit'>Login</Button>
       </div>
+      {errorMsg && <div className='error-msg'>{errorMsg}</div>}
     </form>
   );
 };
--- src/components/UI/Input/index.jsx
+++ src/components/UI/Input/index.jsx
@@ -6,16 +6,26 @@ const Input = ({
   inputId,
   placeholder,
   labelText,
+  refValue,
+  value,
+  onChange,
+  required,
+  hasError,
   autoComplete = 'off',
 }) => {
   return (
     <div className='input'>
       {labelText && <label htmlFor={inputId}>{labelText}</label>}
       <input
+        className={`${hasError ? 'error' : ''}`}
+        ref={refValue}
         type={type}
         id={inputId}
         placeholder={placeholder}
         autoComplete={autoComplete}
+        value={value}
+        onChange={onChange}
+        required={required}
       />
     </div>
   );
