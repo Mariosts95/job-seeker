a5d6dbd2bbbb41f2686a1bb0a0de400f4a7d62b3
Mariosts95 <marios.tsioutsis1@gmail.com>
2022-04-05 21:13:30 +0300

    Add and setup react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+// import react router
+import { Routes, Route } from 'react-router-dom';
+
 // import views
 import Login from './screens/Login';
 import JobPosts from './screens/JobPosts';
 import Success from './screens/Success';
+import Error404 from './screens/Error404';
 
 // import components
 import Header from './components/UI/Header';
@@ -13,9 +17,12 @@ const App = () => {
   return (
     <>
       <Header />
-      <Login />
-      <JobPosts />
-      <Success />
+      <Routes>
+        <Route path='/' index element={<Login />} />
+        <Route path='/jobs' element={<JobPosts />} />
+        <Route path='/success' element={<Success />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
     </>
   );
 };
