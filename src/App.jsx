c63e6295e705eacb75ee554264100d8ac8184b9d
Marios Tsioutsis <72659932+Mariosts95@users.noreply.github.com>
2022-04-05 21:15:19 +0300

    Revert "Feat/react router"

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,45 @@
-// import react router
-import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
 
-// import views
-import Login from './screens/Login';
-import JobPosts from './screens/JobPosts';
-import Success from './screens/Success';
-import Error404 from './screens/Error404';
+function App() {
+  const [count, setCount] = useState(0)
 
-// import components
-import Header from './components/UI/Header';
-
-// import styles
-import './sass/index.scss';
-
-const App = () => {
   return (
-    <>
-      <Header />
-      <Routes>
-        <Route path='/' index element={<Login />} />
-        <Route path='/jobs' element={<JobPosts />} />
-        <Route path='/success' element={<Success />} />
-        <Route path='*' element={<Error404 />} />
-      </Routes>
-    </>
-  );
-};
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button type="button" onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.jsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className="App-link"
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  )
+}
 
-export default App;
+export default App
